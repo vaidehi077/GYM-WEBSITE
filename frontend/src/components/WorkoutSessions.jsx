d040238cc4a6c9c95e6d7aa65ef5ff9c4b887ee5
vaidehi077 <vaishnavvaidehi03@gmail.com>
2024-06-03 08:41:40 +0530

    Workout Session file is updated

--- frontend/src/components/WorkoutSessions.jsx
+++ frontend/src/components/WorkoutSessions.jsx
@@ -4,14 +4,13 @@ function WorkoutSessions() {
   return (
    <section className='workout_session'> 
      <div className="wrapper">
-        <h1>TOP WORKOUT SESSION</h1>
+        <h1 className='WorkOut-Session-heading'>TOP WORKOUT SESSION</h1>
         <p><ul>
-         <li> <b>Personalized Attention:</b> Our trainers provide personalized modifications and attention to ensure you get the most out of every workout.</li>
-         <li><b>State-of-the-Art Equipment: </b>Utilize the latest fitness equipment designed to maximize your workout efficiency.</li>
-         <li><b>Progress Tracking:</b> Track your progress with regular assessments to see how far you've come and set new goals.</li>
-         <li><b>Nutritional Guidance:</b> Receive expert advice on nutrition to complement your workout regime and accelerate your results.</li>
+         <li> <b>Personalized Attention:</b> Our trainers provide personalized modifications and attention to ensure you get the most out of every workout.</li><br />
+         <li><b>Progress Tracking:</b> Track your progress with regular assessments to see how far you've come and set new goals.</li><br />
+         <li><b>Nutritional Guidance:</b> Receive expert advice on nutrition to complement your workout regime and accelerate your results.</li><br />
         </ul></p>
-        <img src="/img5.jpg" alt="workout" className='WorkoutSessio'/>
+        <img src="/img5.jpg" alt="workout" className='WorkoutSessionImg'/>
      </div>
      <div className="wrapper">
         <h1>FEATURED BOOTCAMP</h1>
@@ -19,25 +18,18 @@ function WorkoutSessions() {
         <div> <h4>High-Intensity Training:</h4>
         <p>Engage in high-energy workouts that burn calories and build strength.</p>
         </div>
-        <div> <h4>High-Intensity Training:</h4>
-        <p>Engage in high-energy workouts that burn calories and build strength.</p>
+        <div> <h4>Expert Trainers: </h4>
+        <p>Benefit from the guidance of certified trainers who tailor the workouts to your fitness level and goals.</p>
         </div>
-        <div> <h4>High-Intensity Training:</h4>
-        <p>Engage in high-energy workouts that burn calories and build strength.</p>
+        <div> <h4>Dynamic Routines:</h4>
+        <p>Experience a variety of exercises that keep your workouts fresh and exciting, from strength training to cardio bursts.</p>
         </div>
-        <div> <h4>High-Intensity Training:</h4>
-        <p>Engage in high-energy workouts that burn calories and build strength.</p>
+        <div> <h4>Community Support: </h4>
+        <p>Join a supportive group of fitness enthusiasts who motivate and inspire each other.</p>
         </div>
-        <div> <h4>High-Intensity Training:</h4>
-        <p>Engage in high-energy workouts that burn calories and build strength.</p>
+        <div> <h4>State-of-the-Art Equipment:</h4>
+        <p>Utilize the latest fitness equipment designed to maximize your workout efficiency.</p>
         </div>
-        <div> <h4>High-Intensity Training:</h4>
-        <p>Engage in high-energy workouts that burn calories and build strength.</p>
-        </div>
-        <div> <h4>High-Intensity Training:</h4>
-        <p>Engage in high-energy workouts that burn calories and build strength.</p>
-        </div>
-        
      </div>
      </div>
    </section>
