b41b6814d04addf877c73eee73f470f81986b74b
PurviMurkute <purvimurkute51@gmail.com>
2024-06-04 18:03:22 +0530

    signup and login page added

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,73 @@
+import {useState} from 'react';
+import {Link} from "react-router-dom";
+import axios from 'axios'
+
+function signup() {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:4000/register', {name, email, password})
+        .then(result => console.log(result))
+        .catch(err=> console.log(err))
+    }
+  return (
+    <div className='d-flex justify-content-center align-items-center bg-black bg-gradient vh-100'>
+        <div className='bg-white p-3 rounded w-25'>
+            <h2 className='text-center'>Register</h2><br/>
+            <form onSubmit={handleSubmit}>
+                <div className='mb-3'>
+                    <label htmlfor='email'>
+                        <strong>Name</strong>
+                    </label>
+                    <input
+                    type='text'
+                    placeholder='Enter Name'
+                    autoComplete='off'
+                    name='email'
+                    className='form-control rounded-0'
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='email'>
+                        <strong>Email</strong>
+                    </label>
+                    <input
+                    type='email'
+                    placeholder='Enter Email'
+                    autoComplete='off'
+                    name='email'
+                    className='form-control rounded-0'
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='email'>
+                        <strong>Password</strong>
+                    </label>
+                    <input
+                    type='password'
+                    placeholder='Enter Password'
+                    name='password'
+                    className='form-control rounded-0'
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type='submit' className='btn btn-success w-100 rounded-0'>
+                    Register
+                </button>
+            </form>
+            <p>Already Have an Account</p>
+            <Link to='/login' className='btn btn-default border w-100 bg-light rounded-o text-decoration-underline'>
+                Login
+            </Link>
+        </div>
+
+    </div>
+  )
+}
+
+export default signup
