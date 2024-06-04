1f89e010fd7cff5f2657cfe2018f462c82ee8380
vaidehi077 <vaishnavvaidehi03@gmail.com>
2024-06-04 08:12:13 +0530

    New Changes Added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,6 @@ const Footer = () => {
           <h4>Quick Links</h4>
           <ul>
             <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#classes">Classes</a></li>
             <li><a href="#contact">Contact</a></li>
