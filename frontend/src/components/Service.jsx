f13a9b1592e26ac226c590c23baacb66a22c4f88
Aashika Jain <jainaashika1510@gmail.com>
2024-06-04 10:13:06 +0530

    Services first row added

--- frontend/src/components/Service.jsx
+++ frontend/src/components/Service.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Service() {
   return (
-    <div></div>
+    <div>
+        <h1 className='text-light text-center text-decoration-underline'>Our Services</h1>
+      <div className="d-flex mt-5">
+        {
+          CARD.map((cardObj) => {
+
+            const { s, title } = cardObj;
+            console.log(cardObj);
+            return <Cards s={s} title={title}
+            />
+          })
+        }</div>
+    </div>
   )
 }
 
