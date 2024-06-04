337c0303b64a574e5e14f0c06a1a8ed606c1829c
Aashika Jain <jainaashika1510@gmail.com>
2024-06-04 10:22:56 +0530

    first row config updated

---
+++ frontend/src/components/config.jsx
@@ -0,0 +1,19 @@
+const CARD = [
+    {
+        s: "ptrainer.jpg",
+        title: "Personal Trainer"
+    },
+    {
+        s: "grp.jpg",
+        title: "Group Training"
+
+    },
+    {
+        s: "gym.jpg",
+        title: "Modern Equipments"
+    },
+    {
+        s: "diet-plan.png",
+        title: "Personal Diet Plan"
+    },
+];
